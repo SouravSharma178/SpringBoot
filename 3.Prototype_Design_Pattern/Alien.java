c99195fa57d9e90bbe90fa897cf5024599a36c1a
SouravSharma178 <104345909+SouravSharma178@users.noreply.github.com>
2022-09-20 20:35:53 +0530

    Create Alien.java

---
+++ 3.Prototype_Design_Pattern/Alien.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+// the moment we write @Component it is a spring bean and we can use it anywhere
+// if we want to make an object only when we explicitly specify,then we must use an annotation class scope and an annotation called prototype
+@Component
+@Scope(value="prototype")
+public class Alien {
+    private int aid;
+    private String aname;
+    private String tech;
+    
+	public Alien() {
+		super();
+		System.out.println("Object created twice (* when using a prototype design pattern *)");
+	}
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+    void show () {
+    	System.out.println("This is the show method");
+    }
+}
