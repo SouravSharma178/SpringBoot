48b43640783394ba1b77f8f88991c719fef6abac
SouravSharma178 <104345909+SouravSharma178@users.noreply.github.com>
2022-09-22 03:09:49 +0530

    Create HomeController.java

---
+++ 12.REST_Fetch_BY_Id/HomeController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import com.example.demo.dao.AlienRepo;
+import com.example.demo.model.Alien;
+
+
+@Controller
+public class HomeController {
+
+@Autowired
+AlienRepo repo;
+@RequestMapping("/home")
+public String home() {
+	
+	return "home.jsp";
+}
+@RequestMapping("/addAlien")
+public String addAlien(Alien alien) {
+	
+	repo.save(alien);
+	return "home.jsp";
+}
+
+@RequestMapping("/aliens")
+@ResponseBody
+// since we are using REST here we are going to use @ResponseBody
+public String getAliens() {
+	
+// the repo.findAll() is an iterable by default so we need to convert it to a string
+return repo.findAll().toString();
+}
+
+@RequestMapping("/alien/{aid}")
+@ResponseBody
+
+public String getAlien(@PathVariable("aid") int aid) {
+	
+return repo.findById(aid).toString();
+}
+
+}
