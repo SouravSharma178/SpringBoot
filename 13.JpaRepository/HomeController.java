2eb1f9c7e46493910b831e93251b0130136b33f4
SouravSharma178 <104345909+SouravSharma178@users.noreply.github.com>
2022-09-22 03:20:26 +0530

    Create HomeController.java

---
+++ 13.JpaRepository/HomeController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.demo.dao.AlienRepo;
+import com.example.demo.model.Alien;
+
+
+@Controller
+public class HomeController {
+
+@Autowired
+AlienRepo repo;
+@RequestMapping("/home")
+public String home() {
+	
+	return "home.jsp";
+}
+@RequestMapping("/addAlien")
+public String addAlien(Alien alien) {
+	
+	repo.save(alien);
+	return "home.jsp";
+}
+
+@RequestMapping("/aliens")
+@ResponseBody
+// since we are using REST here we are going to use @ResponseBody
+public List <Alien> getAliens() {
+	
+// the repo.findAll() is an iterable by default so we need to convert it to a string
+return repo.findAll();
+}
+
+@RequestMapping("/alien/{aid}")
+@ResponseBody
+
+public Optional <Alien> getAlien(@PathVariable("aid") int aid) {
+	
+return repo.findById(aid);
+}
+
+}
